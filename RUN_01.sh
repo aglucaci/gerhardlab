a682b6982576107fe56c5d394c0b603d3d3d8b7c
Alexander Lucaci <43097993+aglucaci@users.noreply.github.com>
2019-04-24 20:56:22 +0000

    Create RUN_01.sh
    initial commit

---
+++ RUN_01.sh
@@ -0,0 +1,37 @@
+## REPOSITORY (RQ534361)
+## PROJECT NAME - KA
+## INVITAE PAIRED END RUNS 
+## FASTQ FILES
+#SQ6981_S1_L001_R1_001.fastq.gz
+#SQ6981_S1_L002_R1_001.fastq.gz 
+#SQ6981_S1_L003_R1_001.fastq.gz 
+#SQ6981_S1_L004_R1_001.fastq.gz
+#SQ6981_S1_L001_R2_001.fastq.gz
+#SQ6981_S1_L002_R2_001.fastq.gz 
+#SQ6981_S1_L003_R2_001.fastq.gz 
+#SQ6981_S1_L004_R2_001.fastq.gz
+
+clear
+
+echo . 
+
+#COMBINE RUNS INTO LARGER FASTQ
+cat SQ6981_S1_L001_R1_001.fastq.gz SQ6981_S1_L002_R1_001.fastq.gz SQ6981_S1_L003_R1_001.fastq.gz SQ6981_S1_L004_R1_001.fastq.gz > SQ6981_S1_L00X_MASTER_R1_001.fastq.gz
+cat SQ6981_S1_L001_R2_001.fastq.gz SQ6981_S1_L002_R2_001.fastq.gz SQ6981_S1_L003_R2_001.fastq.gz SQ6981_S1_L004_R2_001.fastq.gz > SQ6981_S1_L00X_MASTER_R2_001.fastq.gz
+
+#RUN FASTQC
+fastqc SQ6981_S1_L00X_MASTER_R1_001.fastq.gz
+fastqc SQ6981_S1_L00X_MASTER_R2_001.fastq.gz
+
+#Mapping
+#Reference downloaded Illumina iGenomes hg19
+#Reference Directory: /media/alexander/Elements/Homo_sapiens_UCSC_hg19/Homo_sapiens/UCSC/hg19/Sequence/WholeGenomeFasta
+#/media/alexander/Elements/Homo_sapiens_UCSC_hg19/Homo_sapiens/UCSC/hg19/Sequence/WholeGenomeFasta/genome.fa
+
+bwa index genome.fa
+
+#Reads Directory: /media/alexander/Elements/RQ534361(KA)/Data
+#/media/alexander/Elements/RQ534361(KA)/Data/SQ6981_S1_L00X_MASTER_R1_001.fastq.gz
+#/media/alexander/Elements/RQ534361(KA)/Data/SQ6981_S1_L00X_MASTER_R2_001.fastq.gz
+
+bwa mem -M -t 7 /media/alexander/Elements/Homo_sapiens_UCSC_hg19/Homo_sapiens/UCSC/hg19/Sequence/WholeGenomeFasta/genome.fa /media/alexander/Elements/RQ534361-KA/Data/SQ6981_S1_L00X_MASTER_R1_001.fastq.gz /media/alexander/Elements/RQ534361-KA/Data/SQ6981_S1_L00X_MASTER_R2_001.fastq.gz > aligned_SQ6981.sam
