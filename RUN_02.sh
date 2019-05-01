f4b608d0283f0a9ade1fb48eb5e9262c7877997d
Alexander Lucaci <43097993+aglucaci@users.noreply.github.com>
2019-05-01 19:03:47 +0000

    Update RUN_02.sh
    added the bcftools mpileup piped into varscan (snp and indel)

--- RUN_02.sh
+++ RUN_02.sh
@@ -167,11 +167,15 @@ bcftools call -v -m bcftools_aligned_markdup_SQ6981.mpileup > bcftools_aligned_m
 #VarScan - http://varscan.sourceforge.net/using-varscan.html
 #v2.4.3 seems to not like samtools mpileup, using bcftools
 #Input: bcftools mpileup
+
 #varscan mpileup2snp samtools_aligned_markdup_SQ6981.mpileup --min-coverage 30 --output-vcf 1 > varscan_samtools_aligned_markdup_SQ6981.mpileup.vcf
-varscan mpileup2snp bcftools_aligned_markdup_SQ6981.mpileup --min-coverage 30 --output-vcf 1 > varscan_bcftools_aligned_markdup_SQ6981.mpileup.vcf
+#varscan mpileup2snp bcftools_aligned_markdup_SQ6981.mpileup --min-coverage 30 --output-vcf 1 > varscan_bcftools_aligned_markdup_SQ6981.mpileup.vcf
+#bcftools mpileup --threads 6 -Ou -f /media/alexander/Elements/Homo_sapiens_UCSC_hg19/Homo_sapiens/UCSC/hg19/Sequence/WholeGenomeFasta/genome.fa aligned_markdup_SQ6981.bam | varscan mpileup2indel > varscan_indel_bcftools_aligned_markdup_SQ6981.mpileup.vcf 
+bcftools mpileup -uf /media/alexander/Elements/Homo_sapiens_UCSC_hg19/Homo_sapiens/UCSC/hg19/Sequence/WholeGenomeFasta/genome.fa aligned_markdup_SQ6981.bam | varscan mpileup2snp > varscan_snp_bcftools_aligned_markdup_SQ6981.mpileup.vcf
 
 #varscan mpileup2indel samtools_aligned_markdup_SQ6981.mpileup --min-coverage 30
-varscan mpileup2indel bcftools_aligned_markdup_SQ6981.mpileup --min-coverage 30
+#varscan mpileup2indel bcftools_aligned_markdup_SQ6981.mpileup --min-coverage 30
+bcftools mpileup -uf /media/alexander/Elements/Homo_sapiens_UCSC_hg19/Homo_sapiens/UCSC/hg19/Sequence/WholeGenomeFasta/genome.fa aligned_markdup_SQ6981.bam | varscan mpileup2indel > varscan_indel_bcftools_aligned_markdup_SQ6981.mpileup.vcf
 
 #VarScan - Combine the varscan vcf's
 
