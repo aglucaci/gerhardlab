8b372ed5947947f285938865d1941f9415802b42
Alexander Lucaci <43097993+aglucaci@users.noreply.github.com>
2019-05-02 15:29:32 +0000

    Create GATK_Compatible.sh
    initial commit

---
+++ GATK_Compatible.sh
@@ -0,0 +1,25 @@
+#Whole exome to variant calling
+#Following the GATK4 Best Practices
+#https://software.broadinstitute.org/gatk/best-practices/workflow?id=11145
+
+#PATH VARIABLE
+
+#FILES
+
+##########################################################
+# Mapping
+##########################################################
+
+#Reference downloaded Illumina iGenomes hg19
+#Reference Directory: /media/alexander/Elements/Homo_sapiens_UCSC_hg19/Homo_sapiens/UCSC/hg19/Sequence/WholeGenomeFasta
+#/media/alexander/Elements/Homo_sapiens_UCSC_hg19/Homo_sapiens/UCSC/hg19/Sequence/WholeGenomeFasta/genome.fa
+
+#Index our hg19 genome fasta (take a while.)
+#Version: 0.7.17-r1188
+bwa index genome.fa
+
+b#wa mem -K 100000000 -p -v 3 -t 16 -Y $bash_ref_fasta
+
+#bwa mem -M -t 7 /media/alexander/Elements/Homo_sapiens_UCSC_hg19/Homo_sapiens/UCSC/hg19/Sequence/WholeGenomeFasta/genome.fa /media/alexander/Elements/RQ534361-KA/Data/SQ6981_S1_L00X_MASTER_R1_001.fastq.gz /media/alexander/Elements/RQ534361-KA/Data/SQ6981_S1_L00X_MASTER_R2_001.fastq.gz > aligned_M_SQ6981.sam
+
+bwa mem -K 100000000 -p -v 3 -t 6 /media/alexander/Elements/Homo_sapiens_UCSC_hg19/Homo_sapiens/UCSC/hg19/Sequence/WholeGenomeFasta/genome.fa /media/alexander/Elements/RQ534361-KA/Data/SQ6981_S1_L00X_MASTER_R1_001.fastq.gz /media/alexander/Elements/RQ534361-KA/Data/SQ6981_S1_L00X_MASTER_R2_001.fastq.gz > SQ6981.aligned.unsorted.sam
