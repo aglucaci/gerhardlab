7280af85d4f4045cdec51c224d2b0aa1389d8aa6
Alexander Lucaci <43097993+aglucaci@users.noreply.github.com>
2019-05-02 15:34:05 +0000

    Update GATK_Compatible.sh
    update bwa mem command

--- GATK_Compatible.sh
+++ GATK_Compatible.sh
@@ -18,8 +18,8 @@
 #Version: 0.7.17-r1188
 bwa index genome.fa
 
-b#wa mem -K 100000000 -p -v 3 -t 16 -Y $bash_ref_fasta
-
+#bwa mem -K 100000000 -p -v 3 -t 16 -Y $bash_ref_fasta
 #bwa mem -M -t 7 /media/alexander/Elements/Homo_sapiens_UCSC_hg19/Homo_sapiens/UCSC/hg19/Sequence/WholeGenomeFasta/genome.fa /media/alexander/Elements/RQ534361-KA/Data/SQ6981_S1_L00X_MASTER_R1_001.fastq.gz /media/alexander/Elements/RQ534361-KA/Data/SQ6981_S1_L00X_MASTER_R2_001.fastq.gz > aligned_M_SQ6981.sam
 
-bwa mem -K 100000000 -p -v 3 -t 6 /media/alexander/Elements/Homo_sapiens_UCSC_hg19/Homo_sapiens/UCSC/hg19/Sequence/WholeGenomeFasta/genome.fa /media/alexander/Elements/RQ534361-KA/Data/SQ6981_S1_L00X_MASTER_R1_001.fastq.gz /media/alexander/Elements/RQ534361-KA/Data/SQ6981_S1_L00X_MASTER_R2_001.fastq.gz > SQ6981.aligned.unsorted.sam
+cd /media/alexander/Elements/RQ534361-KA/Analysis/
+bwa mem -K 100000000 -p -v 3 -t 6 /media/alexander/Elements/Homo_sapiens_UCSC_hg19/Homo_sapiens/UCSC/hg19/Sequence/WholeGenomeFasta/genome.fa SQ6981_S1_L00X_MASTER_R1_001.fastq.gz SQ6981_S1_L00X_MASTER_R2_001.fastq.gz > SQ6981.aligned.unsorted.sam
