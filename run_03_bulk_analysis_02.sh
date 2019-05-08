b534647611b6b36231cee468ca97ed941c1be003
Alexander Lucaci <43097993+aglucaci@users.noreply.github.com>
2019-05-08 01:48:26 +0000

    Add files via upload

--- run_03_bulk_analysis_02.sh
+++ run_03_bulk_analysis_02.sh
@@ -6,6 +6,9 @@ GATK="/home/alexander/Downloads/gatk-4.1.1.0/gatk-package-4.1.1.0-local.jar"
 REFERENCE="/media/alexander/Elements/Homo_sapiens_UCSC_hg19/Homo_sapiens/UCSC/hg19/Sequence/WholeGenomeFasta/genome.fa"
 GNOMAD_VCF="/media/alexander/Elements/gnomad.exomes.r2.1.1.sites.vcf.bgz"
 
+READ1="SQ6981_S1_L00X_MASTER_R1_001.fastq.gz"
+READ2="SQ6981_S1_L00X_MASTER_R2_001.fastq.gz"
+
 WD = ""
 #######################################################################
 # Initialize this script.
@@ -25,11 +28,13 @@ echo "Changing directory to Data"
 cd ../Data
 
 echo "Combining FASTQ files.."
-cat SQ6981_S1_L001_R1_001.fastq.gz SQ6981_S1_L002_R1_001.fastq.gz SQ6981_S1_L003_R1_001.fastq.gz SQ6981_S1_L004_R1_001.fastq.gz > ../Analysis/Run_03/SQ6981_S1_L00X_MASTER_R1_001.fastq.gz
-cat SQ6981_S1_L001_R2_001.fastq.gz SQ6981_S1_L002_R2_001.fastq.gz SQ6981_S1_L003_R2_001.fastq.gz SQ6981_S1_L004_R2_001.fastq.gz > ../Analysis/Run_03/SQ6981_S1_L00X_MASTER_R2_001.fastq.gz
+if [[ ! -e "../Analysis/Run_03/"$READ1 ]]
+then
+    cat SQ6981_S1_L001_R1_001.fastq.gz SQ6981_S1_L002_R1_001.fastq.gz SQ6981_S1_L003_R1_001.fastq.gz SQ6981_S1_L004_R1_001.fastq.gz > ../Analysis/Run_03/SQ6981_S1_L00X_MASTER_R1_001.fastq.gz
+    cat SQ6981_S1_L001_R2_001.fastq.gz SQ6981_S1_L002_R2_001.fastq.gz SQ6981_S1_L003_R2_001.fastq.gz SQ6981_S1_L004_R2_001.fastq.gz > ../Analysis/Run_03/SQ6981_S1_L00X_MASTER_R2_001.fastq.gz
+fi
 
-READ1="SQ6981_S1_L00X_MASTER_R1_001.fastq.gz"
-READ2="SQ6981_S1_L00X_MASTER_R2_001.fastq.gz"
+#if [ ! -f "/etc/bebebe" ]; then echo "The file does not exist"; fi
 
 echo "Changing directory to Data to ../Analysis/Run_03"
 cd ../Analysis/Run_03
@@ -45,9 +50,11 @@ cd ../Analysis/Run_03
 # Run FASTQC
 ##########################################################
 #Version 0.11.5
-echo "Generating FASTQC files"
-fastqc $READ1
-fastqc $READ2
+#echo "Generating FASTQC files"
+#pwd
+#fastqc $READ1
+#fastqc $READ2
+#Giving me a Java 9/8 error. See how to resolve this.
 
 ##########################################################
 # Mapping 
@@ -56,31 +63,37 @@ fastqc $READ2
 #bwa index $REFERENCE
 
 echo "Mapping reads to reference"
-bwa mem -M -t 7 -R "@RG\tID:SQ6981\tSM:SQ6981\tPL:Illumina" $REFERENCE $READ1 $READ2 > aligned_SQ6981.sam 
+if [[ ! -e "aligned_SQ6981.sam" ]]
+then
+    bwa mem -M -t 7 -R "@RG\tID:SQ6981\tSM:SQ6981\tPL:Illumina" $REFERENCE $READ1 $READ2 > aligned_SQ6981.sam 
+fi
 
 ##########################################################
 # Prepping for analysis 
 ##########################################################
 echo "Converting SAM to BAM"
-samtools view -bS aligned_SQ6981.sam > aligned_SQ6981.bam
+if [[ ! -e "aligned_SQ6981.bam" ]]
+then
+    samtools view -bS aligned_SQ6981.sam > aligned_SQ6981.bam
+fi
+
+echo "Sorting BAM"
+java -jar $PICARD SortSam I=aligned_SQ6981.bam O=sorted_aligned_SQ6981.bam SORT_ORDER=coordinate
 
 echo "Marking PCR Duplicates"
 #https://software.broadinstitute.org/gatk/best-practices/workflow?id=11165
-java -jar $PICARD MarkDuplicates I=aligned_SQ6981.bam O=marked_duplicates_aligned_SQ6981.bam M=marked_dup_metrics.txt
-
-echo "Sorting BAM"
-java -jar $PICARD SortSam I=marked_duplicates_aligned_SQ6981.bam O=sorted_marked_duplicates_aligned_SQ6981.bam SORT_ORDER=coordinate
+java -jar $PICARD MarkDuplicates I=sorted_aligned_SQ6981.bam O=marked_duplicates_sorted_aligned_SQ6981.bam M=marked_dup_metrics.txt
 
 echo "Generated recalibration table based on gnomAD"
-java -jar $GATK BaseRecalibrator -I sorted_marked_duplicates_aligned_SQ6981.bam -R $REFERENCE --known-sites $GNOMAD_VCF -O recal_data.table
+java -jar $GATK BaseRecalibrator -I marked_duplicates_sorted_aligned_SQ6981.bam -R $REFERENCE --known-sites $GNOMAD_VCF -O recal_data.table
 
 echo "Applying base recalibration"
-java -jar $GATK ApplyBQSR -R $REFERENCE -I sorted_marked_duplicates_aligned_SQ6981.bam --bqsr-recal-file recal_data.table -O BQSR_sorted_marked_duplicates_aligned_SQ6981.bam
+java -jar $GATK ApplyBQSR -R $REFERENCE -I marked_duplicates_sorted_aligned_SQ6981.bam --bqsr-recal-file recal_data.table -O BQSR_marked_duplicates_sorted_aligned_SQ6981.bam
 
 ##########################################################
 # Variant Calling
 ##########################################################
-QUERY="BQSR_sorted_marked_duplicates_aligned_SQ6981.bam"
+QUERY="BQSR_marked_duplicates_sorted_aligned_SQ6981.bam"
 
 freebayes -C 30 -f $REFERENCE $QUERY > Freebayes_30X_BQSR_sorted_marked_duplicates_aligned_SQ6981.bam.vcf
 
@@ -90,7 +103,7 @@ samtools mpileup -f $REFERENCE $QUERY | varscan mpileup2snp --min-coverage 30 --
 
 samtools mpileup -f $REFERENCE $QUERY | varscan mpileup2indel --min-coverage 30 --output-vcf 1 > varscan_indel_30X_samtools_BQSR_sorted_marked_duplicates_aligned_SQ6981.bam.mpileup.vcf
 
-java -jar $GATK "-Xmx4g" HaplotypeCaller -R $REFERENCE-I $QUERY -O GATK_BQSR_sorted_marked_duplicates_aligned_SQ6981.bam.vcf.gz -bamout GATK_bamout.bam
+java -jar $GATK --java-options "-Xmx4g" HaplotypeCaller -R $REFERENCE-I $QUERY -O GATK_BQSR_sorted_marked_duplicates_aligned_SQ6981.bam.vcf.gz -bamout GATK_bamout.bam
 
 ##########################################################
 # Consolidate VCFs
