bd59bf276aac9f216958275e8d9365be811f9750
Alexander Lucaci <43097993+aglucaci@users.noreply.github.com>
2019-05-10 13:11:04 -0400

    Add files via upload

--- VRTX_RQ553841_Ca2+.sh
+++ VRTX_RQ553841_Ca2+.sh
@@ -1,24 +1,30 @@
 
 FASTQC="/home/alexander/Downloads/FastQC/fastqc"
+REFERENCE=""
 WD="/run/media/alexander/4TB-VD1/Projects/RQ553841_Ca2+/Scripts"
+
+#Initialize
+
 cd $WD
 
 cd ../Data
 
-## Files
+## -- Files
 #SQ7711_S1_L001_R1_001.fastq
 #SQ7711_S1_L001_R2_001.fastq
 #SQ7711_S1_L002_R1_001.fastq
 #SQ7711_S1_L002_R2_001.fastq
 
-
+# -- SHA CHECK
 #sha256sum SQ7711_S1_L001_R1_001.fastq.gz
 #sha256sum SQ7711_S1_L001_R2_001.fastq.gz
 #sha256sum SQ7711_S1_L002_R1_001.fastq.gz
 #sha256sum SQ7711_S1_L002_R2_001.fastq.gz
 
-
+# -- FASTQC
 $FASTQC SQ7711_S1_L001_R1_001.fastq.gz
 $FASTQC SQ7711_S1_L001_R2_001.fastq.gz
 $FASTQC SQ7711_S1_L002_R1_001.fastq.gz
 $FASTQC SQ7711_S1_L002_R2_001.fastq.gz
+
+
