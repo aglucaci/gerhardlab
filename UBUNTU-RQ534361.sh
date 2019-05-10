f1e17991e9fe522b33a98cbde3611c251e4b2a29
Alexander Lucaci <43097993+aglucaci@users.noreply.github.com>
2019-05-10 16:49:34 -0400

    Add files via upload

--- UBUNTU-RQ534361.sh
+++ UBUNTU-RQ534361.sh
@@ -27,7 +27,10 @@ PICARD="/home/alexander/Downloads/picard.jar"
 GATK="/home/alexander/Downloads/gatk-4.1.1.0/gatk-package-4.1.1.0-local.jar"
 #cat genome_test.fa | perl -pe 's/chr//g' > genome_chr_replaced.fa
 #Had to replace chr1 to 1 so that the gnomad VCF agrees with the reference, gnomAD uses hg19.
+
 REFERENCE="/media/alexander/Elements/Homo_sapiens_UCSC_hg19/Homo_sapiens/UCSC/hg19/Sequence/WholeGenomeFasta/genome.fa"
+REFERENCE_BWA
+
 REFERENCE_BWA_INDEX="/media/alexander/Elements/Homo_sapiens_UCSC_hg19/Homo_sapiens/UCSC/hg19/Sequence/WholeGenomeFasta/genome.fa.bwt"
 REFERENCE_SAMTOOLS_INDEX="/media/alexander/Elements/Homo_sapiens_UCSC_hg19/Homo_sapiens/UCSC/hg19/Sequence/WholeGenomeFasta/genome.fa.fai"
 REFERENCE_DICT="/media/alexander/Elements/Homo_sapiens_UCSC_hg19/Homo_sapiens/UCSC/hg19/Sequence/WholeGenomeFasta/genome.dict"
@@ -39,7 +42,7 @@ READ2="SQ6981_S1_L00X_MASTER_R2_001.fastq.gz"
 
 echo "Set WD:"
 WD = "/media/alexander/Elements/RQ534361-KA/Scripts"
-cd WD
+cd $WD
 
 echo $WD
 
@@ -54,13 +57,15 @@ echo "## by Alexander G. Lucaci"
 echo "## Dataset: RQ534361-KA"
 echo ""
 
+#echo "Number of Steps:"
+
 #######################################################################
 # Data preprocessing
 #######################################################################
-echo "Changing directory to ../Data"
+echo "() Changing directory to ../Data"
 cd ../Data
 
-echo "Combining FASTQ files.."
+echo "() Combining FASTQ files.."
 if [[ ! -e "../Analysis/Run_03/"$READ1 ]]
 then
     cat SQ6981_S1_L001_R1_001.fastq.gz SQ6981_S1_L002_R1_001.fastq.gz SQ6981_S1_L003_R1_001.fastq.gz SQ6981_S1_L004_R1_001.fastq.gz > ../Analysis/Run_03/SQ6981_S1_L00X_MASTER_R1_001.fastq.gz
@@ -71,7 +76,7 @@ then
     cat SQ6981_S1_L001_R2_001.fastq.gz SQ6981_S1_L002_R2_001.fastq.gz SQ6981_S1_L003_R2_001.fastq.gz SQ6981_S1_L004_R2_001.fastq.gz > ../Analysis/Run_03/SQ6981_S1_L00X_MASTER_R2_001.fastq.gz
 fi
 
-echo "Changing directory to Data to ../Analysis/Run_03"
+echo "() Changing directory to Data to ../Analysis/Run_03"
 cd ../Analysis/Run_03
 
 ##########################################################
@@ -94,14 +99,14 @@ cd ../Analysis/Run_03
 ##########################################################
 # Mapping 
 ##########################################################
-echo "Indexing reference genome"
+echo "() Indexing reference genome"
 if [[ ! -e $REFERENCE_BWA_INDEX ]]
 then
     bwa index $REFERENCE
     #continue
 fi
 
-echo "Mapping reads to reference"
+echo "() Mapping reads to reference"
 if [[ ! -e "aligned_SQ6981.sam" ]]
 then
     bwa mem -M -t 7 -R "@RG\tID:SQ6981\tSM:SQ6981\tPL:Illumina" $REFERENCE $READ1 $READ2 > aligned_SQ6981.sam 
@@ -110,7 +115,7 @@ then
     #bwa mem -M -t 7 -R "@RG\tID:SQ6981\tSM:SQ6981\tPL:Illumina" $REFERENCE $READ1 $READ2 | samtools view -bS > aligned_SQ6981.bam
 fi
 
-echo "Converting SAM to BAM"
+echo "() Converting SAM to BAM"
 if [[ ! -e "aligned_SQ6981.bam" ]]
 then
     samtools view -bS aligned_SQ6981.sam > aligned_SQ6981.bam
@@ -119,7 +124,7 @@ fi
 ##########################################################
 # Analyze the Mapping
 ##########################################################
-echo "Generating alignment flagstats"
+echo "() Generating alignment flagstats"
 if [[ ! -e "flagstat_aligned_SQ6981.bam.txt" ]]
 then
     samtools flagstat aligned_SQ6981.bam > flagstat_aligned_SQ6981.bam.txt
@@ -129,20 +134,20 @@ fi
 # Prepping for analysis 
 ##########################################################
 
-echo "Sorting BAM"
+echo "() Sorting BAM"
 #could also do samtools sort
 if [[ ! -e "sorted_aligned_SQ6981.bam" ]]
 then
     java -jar $PICARD SortSam I=aligned_SQ6981.bam O=sorted_aligned_SQ6981.bam SORT_ORDER=coordinate
 fi
 
-echo "BEDTools - Generate a bedgraph of coverage"
+echo "() BEDTools - Generate a bedgraph of coverage"
 if [[ ! -e "aligned_SQ6981.bam.bedgraph" ]]
 then
     bedtools genomecov -ibam sorted_aligned_SQ6981.bam -bg > aligned_SQ6981.bam.bedgraph
 fi
 
-echo "Marking PCR Duplicates"
+echo "() Marking PCR Duplicates"
 if [[ ! -e "marked_duplicates_sorted_aligned_SQ6981.bam" ]]
 then
     java -jar $PICARD MarkDuplicates I=sorted_aligned_SQ6981.bam O=marked_duplicates_sorted_aligned_SQ6981.bam M=marked_dup_metrics.txt
@@ -151,36 +156,33 @@ fi
 #echo "Cleaning up Reference for GATK"
 #cat genome_test.fa | perl -pe 's/chr//g' > genome_chr_replaced.fa
 #Had to replace chr1 to 1 so that the VCF agrees with the reference. gnomAD uses hg19.
-
 #REFERENCE_CHR_REPLACED="/media/alexander/Elements/Homo_sapiens_UCSC_hg19/Homo_sapiens/UCSC/hg19/Sequence/WholeGenomeFasta/genome_chr_replaced.fa"
-
 #echo "samtools faidx on the Reference chr replaced"
 #samtools faidx $REFERENCE_CHR_REPLACED
-
 #echo "Creating new dict"
 #java -jar $PICARD CreateSequenceDictionary R=$REFERENCE_CHR_REPLACED O="/media/alexander/Elements/Homo_sapiens_UCSC_hg19/Homo_sapiens/UCSC/hg19/Sequence/WholeGenomeFasta/genome_chr_replaced.dict"
 
-echo "Indexing the gnomAD VCF"
+echo "() Indexing the gnomAD VCF"
 if [[ ! -e $GNOMAD_VCF_INDEX ]]
 then
     java -jar $GATK IndexFeatureFile -F $GNOMAD_VCF
     continue
 fi
 
-echo "Samtools indexing the Reference fasta"
+echo "() Samtools indexing the Reference fasta"
 if [[ ! -e $REFERENCE_SAMTOOLS_INDEX ]]
 then
     samtools faidx $REFERENCE
 fi
 
-echo "Creating new dict"
+echo "() Creating new dict"
 if [[ ! -e $REFERENCE_DICT ]]
 then
     java -jar $PICARD CreateSequenceDictionary R=$REFERENCE O=$REFERENCE_DICT
     #java -XX:ParallelGCThreads=<num of  threads> -jar <picard-package-name>.jar
 fi
 
-echo "Generated recalibration table based on gnomAD"
+echo "() Generating recalibration table based on gnomAD"
 #https://software.broadinstitute.org/gatk/best-practices/workflow?id=11165
 #https://www.biostars.org/p/312190/
 if [[ ! -e "recal_data.table" ]]
@@ -189,7 +191,7 @@ then
 fi
 
 exit 1
-echo "Applying base recalibration"
+echo "() Applying base recalibration"
 if [[ ! -e "BQSR_marked_duplicates_sorted_aligned_SQ6981.bam" ]]
 then
     java -jar $GATK ApplyBQSR -R $REFERENCE -I marked_duplicates_sorted_aligned_SQ6981.bam --bqsr-recal-file recal_data.table -O BQSR_marked_duplicates_sorted_aligned_SQ6981.bam
@@ -200,26 +202,26 @@ fi
 ##########################################################
 QUERY="BQSR_marked_duplicates_sorted_aligned_SQ6981.bam"
 
-echo "Variant calling with FreeBayes.."
+echo "() Variant calling with FreeBayes.."
 freebayes -C 30 -f $REFERENCE $QUERY > Freebayes_30X_BQSR_sorted_marked_duplicates_aligned_SQ6981.bam.vcf
 
-echo "Variant calling with bcftools.."
+echo "() Variant calling with bcftools.."
 #can switch to samtools mpileup, any difference?
 bcftools mpileup --threads 6 -Ou -f $REFERENCE $QUERY | bcftools call --threads 7 -Ov -mv | bcftools filter -i 'QUAL>20 && DP>30' > bcftools_30X_aligned_markdup_SQ6981.mpileup.vcf
 
-echo "Variant calling with VarScan - SNPs.."
+echo "() Variant calling with VarScan - SNPs.."
 samtools mpileup -f $REFERENCE $QUERY | varscan mpileup2snp --min-coverage 30 --output-vcf 1 > varscan_snp_30X_samtools_BQSR_sorted_marked_duplicates_aligned_SQ6981.bam.mpileup.vcf
 
-echo "Variant calling with VarScan - InDels.."
+echo "() Variant calling with VarScan - InDels.."
 samtools mpileup -f $REFERENCE $QUERY | varscan mpileup2indel --min-coverage 30 --output-vcf 1 > varscan_indel_30X_samtools_BQSR_sorted_marked_duplicates_aligned_SQ6981.bam.mpileup.vcf
 
-echo "Variant calling with GATK - HaplotypeCaller"
+echo "() Variant calling with GATK - HaplotypeCaller"
 java -jar $GATK --java-options "-Xmx4g" HaplotypeCaller -R $REFERENCE-I $QUERY -O GATK_BQSR_sorted_marked_duplicates_aligned_SQ6981.bam.vcf.gz -bamout GATK_bamout.bam
 
 ##########################################################
 # Consolidate VCFs
 ##########################################################
-echo "Concatenating VarScan output into one VCF (SNPs + Indels"
+echo "() Concatenating VarScan output into one VCF (SNPs + Indels"
 #bcftools concat -o 
 
 
@@ -244,3 +246,9 @@ echo "Concatenating VarScan output into one VCF (SNPs + Indels"
 
 
 
+echo "() PIPELINE COMPLETE"
+
+
+##########################################################
+# End of File.
+##########################################################
