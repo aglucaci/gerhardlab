ba9adde62e2e5190e074533a6da555e70b5bc174
Alexander Lucaci <43097993+aglucaci@users.noreply.github.com>
2019-05-10 15:14:45 -0400

    Add files via upload

--- VRTX_RQ553841_Ca2+.sh
+++ VRTX_RQ553841_Ca2+.sh
@@ -9,17 +9,35 @@
 #LINK TO GNOMAD VCF - https://gnomad.broadinstitute.org/downloads
 #https://storage.googleapis.com/gnomad-public/release/2.1.1/vcf/exomes/gnomad.exomes.r2.1.1.sites.vcf.bgz
 
+## Software Versions
+#FASTQC
+#PICARD
+#GATK 4.1.2.0
+#samtools Version: 0.1.19-44428cd
+
+## -- SOFTWARE -- ##
 FASTQC="/home/alexander/Downloads/FastQC/fastqc"
+PICARD="/home/alexander/Downloads/picard.jar"
+GATK=""
+
+
+## Reference files ##
 REFERENCE="/run/media/alexander/4TB-VD1/Projects/REFERENCE/Homo_sapiens_UCSC_hg19/Homo_sapiens/UCSC/hg19/Sequence/WholeGenomeFasta/genome.fa"
 REFERENCE_INDEX="/run/media/alexander/4TB-VD1/Projects/REFERENCE/Homo_sapiens_UCSC_hg19/Homo_sapiens/UCSC/hg19/Sequence/WholeGenomeFasta/genome.fa.fai"
 REFERENCE_DICT="/run/media/alexander/4TB-VD1/Projects/REFERENCE/Homo_sapiens_UCSC_hg19/Homo_sapiens/UCSC/hg19/Sequence/WholeGenomeFasta/genome.dict"
 REFERENCE_BWA="/run/media/alexander/4TB-VD1/Projects/REFERENCE/Homo_sapiens_UCSC_hg19/Homo_sapiens/UCSC/hg19/Sequence/BWAIndex/genome.fa"
-WD="/run/media/alexander/4TB-VD1/Projects/RQ553841_Ca2+/Scripts"
 
+## gnomAD Annotation ##
+GNOMAD_VCF="/run/media/alexander/4TB-VD1/Projects/GNOMAD"
+GNOMAD_INDEX="/run/media/alexander/4TB-VD1/Projects/GNOMAD"
+
+#Set Working Directory
+WD="/run/media/alexander/4TB-VD1/Projects/RQ553841_Ca2+/Scripts"
+cd $WD
+
+#CAT the fastq files together into forward and reverse
 READ1="SQ7711_S1_L00X_COMBINE_R1_001.fastq.gz"
 READ2="SQ7711_S1_L00X_COMBINE_R2_001.fastq.gz"
-
-cd $WD
 
 #######################################################################
 # Initialize this script.
@@ -72,24 +90,45 @@ then
     cat SQ7711_S1_L001_R2_001.fastq.gz SQ7711_S1_L002_R2_001.fastq.gz > ../Analysis/$READ2
 fi
 
-echo "Changing directory to Data to ../Analysis"
+echo "() Changing directory to Data to ../Analysis"
 cd ../Analysis
 
 ##########################################################
 # Mapping 
 ##########################################################
-echo "Mapping reads to reference"
+echo "() Mapping reads to reference"
 
 if [[ ! -e "aligned_SQ7711.sam" ]]
 then
     bwa mem -M -t 25 -R "@RG\tID:SQ7711\tSM:SQ7711\tPL:Illumina" $REFERENCE_BWA $READ1 $READ2 > aligned_SQ7711.sam 
-    #Don't need to store the SAM file: 
-    #on Ubuntu this gave a memory error.
-    #VRTX COMMAND
-    #bwa mem -M -t 25 -R "@RG\tID:SQ7711\tSM:SQ7711\tPL:Illumina" $REFERENCE_BWA $READ1 $READ2 | samtools view -bS > aligned_SQ7711.bam
 fi
 
-#exit 1
+echo "() Converting SAM to BAM"
+if [[ ! -e "aligned_SQ7711.bam" ]]
+then
+    samtools view -bS aligned_SQ7711.sam > aligned_SQ7711.bam
+fi
+
+##########################################################
+# Analyze the Mapping
+##########################################################
+echo "() Generating alignment flagstats"
+if [[ ! -e "flagstat_aligned_SQ7711.bam.txt" ]]
+then
+    samtools flagstat aligned_SQ7711.bam > flagstat_aligned_SQ7711.bam.txt
+fi
+
+##########################################################
+# Prepping for analysis 
+##########################################################
+echo "() Sorting BAM"
+
+if [[ ! -e "sorted_aligned_SQ7711.bam" ]]
+then
+    java -jar $PICARD SortSam I=aligned_SQ7711.bam O=sorted_aligned_SQ7711.bam SORT_ORDER=coordinate
+    #samtools sort aligned_SQ7711.bam > aligned_sorted_SQ7711.bam
+fi
+
 
 #######################################################################
 # End of file
