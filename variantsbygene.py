44a5ab8d141ea2791fcaee78c7800f7ab523c040
Alexander Lucaci <43097993+aglucaci@users.noreply.github.com>
2019-05-11 13:15:13 -0400

    Add files via upload

--- variantsbygene.py
+++ variantsbygene.py
@@ -5,20 +5,34 @@ Created on Fri Mar 15 11:43:25 2019
 
 @author: alexander lucaci
 """
-
+# =============================================================================
+# Imports
+# =============================================================================
 import sys
 import plotly
 import plotly.graph_objs as go
 from plotly import tools
 import numpy as np 
 from scipy import stats
+from prettytable import PrettyTable
+#http://zetcode.com/python/prettytable/
 
+# =============================================================================
+# Declares
+# =============================================================================
 #Annotated VCF by VEP
-fname = "PmhbHNI30Bkw48yH.vcf..txt"
+#path = "/Users/phylo/Documents/Gerhard Lab/RQ534361/"
+#fname = path + "zFSM84RbEYaQM8ck.gnomAD_AF_lt_0.01_and_Consequence_ne_downstream_gene_variant_and_Consequence_ne_upstream_gene_variant_and_Consequence_ne_intron_variant_and_Consequence_ne_synonymous_variant_and_Consequence_ne_non_coding_transcript_exon_var.vcf"
+#path = "/Users/phylo/Documents/Gerhard\ Lab/RQ534361/VEP Analysis 05042019/"
 
+#fname = "jVTFhw6xfxgKfarh.Consequence_ne_downstream_gene_variant_and_Consequence_ne_upstream_gene_variant_and_Consequence_ne_intron_variant_and_Consequence_ne_synonymous_variant_and_Consequence_ne_non_coding_transcript_exon_variant_and_Consequence_n.vcf"
+#fname = "gnomAD_AF_Filtered_jVTFhw6xfxgKfarh_Consequences.vcf"
+fname = "Removed_chrY_gnomADAF_flt_Alex.vcf"
 gene_dict = {}
 
-
+# =============================================================================
+# Helper functions
+# =============================================================================
 def plotly_things2(numbers, filename, desc1, desc2, desc3): #single historgram
     plotly.offline.init_notebook_mode(connected=False)
     plotly.offline.plot({"data": [go.Histogram(x=numbers, text=desc3)],"layout": go.Layout(title=desc2, xaxis={'title':desc1}, yaxis={'title':'Occurences'})},filename=filename+"_HISTOGRAM.html")
@@ -27,7 +41,13 @@ def plotly_things(chromos, numbers, filename, desc1, desc2): #single bar graph
     plotly.offline.init_notebook_mode(connected=False)
     plotly.offline.plot({"data": [go.Bar(x=chromos, y=numbers)],"layout": go.Layout(title=desc2, xaxis={'title':desc1}, yaxis={'title':'Occurences'})},filename=filename+"_HISTOGRAM.html")
 
+def plotly_things_vert(chromos, numbers, filename, desc1, desc2): #single bar graph
+    plotly.offline.init_notebook_mode(connected=False)
+    plotly.offline.plot({"data": [go.Bar(y=chromos, x=numbers)],"layout": go.Layout(title=desc2, xaxis={'title':"Occurences"}, yaxis={'title':desc1})},filename=filename+"_HISTOGRAM.html")
 
+# =============================================================================
+# main sub
+# =============================================================================
 def main(filename):
     global chr_dict
     with open(filename) as f:
@@ -46,7 +66,9 @@ def main(filename):
                     gene_dict[line.split()[7].split("|")[3]] = 1
     f.close()   
     
-    
+# =============================================================================
+# main prog. starts here
+# =============================================================================
 main(fname)
 
 #print(gene_dict)
@@ -68,17 +90,35 @@ for w in sorted(d, key=d.get, reverse=True):
   genes.append(w)
   numbers.append(d[w])
   
-genes[0] = "UNKNOWN"
+#genes[0] = "UNKNOWN"
+
 #genes.pop(0)
 #numbers.pop(0)
 
+print("Filename:", fname)
 print("Number of genes:", len(genes), ",Number of variants:", sum(numbers))
-#plotly_things(genes, numbers, "WES_VCF_RUN_01",  "Gene", "Variants by Gene, (Gene n=" + str(len(genes)) + ") (Variants n=" + str(sum(numbers)) + ")")
+plotly_things(genes, numbers, "WES_VCF_RUN_01",  "Gene", "Variants by Gene, (Gene n=" + str(len(genes)) + ") (Variants n=" + str(sum(numbers)) + ")")
 #print(*genes[:25], sep="\n")
 
+
+x = PrettyTable()
+x.field_names = ["#", "Gene", "Number of variants"]
+                 
 for i, gene in enumerate(genes[:25]):
-    print(i+1, gene)
+    print(i+1, "GENE:", gene, ", Variants:", numbers[i])
+    x.add_row([i+1, gene, numbers[i]])
+print()
+print(x)
+    
+x = PrettyTable()
+x.field_names = ["#", "Gene", "Number of variants"]
   
+                
+for gene in genes: print(gene)
+print(",".join(genes))
+print(" OR ".join(genes))
+
+
 # =============================================================================
 # End of file
 # =============================================================================
--- vcf_filter.py
+++ vcf_filter.py
@@ -12,63 +12,81 @@ import os, sys
 # =============================================================================
 # Declares
 # =============================================================================
-consequence = ["synonymous_variant", "intron_variant", "non_coding_transcript_exon_variant", "upstream_gene_variant", "downstream_gene_variant"]
-fname = "5enxcsqdMxN4a9CH.vcf"
+#consequence = ["synonymous_variant", "intron_variant", "non_coding_transcript_exon_variant", "upstream_gene_variant", "downstream_gene_variant"]
+#fname = "jVTFhw6xfxgKfarh.Consequence_ne_downstream_gene_variant_and_Consequence_ne_upstream_gene_variant_and_Consequence_ne_intron_variant_and_Consequence_ne_synonymous_variant_and_Consequence_ne_non_coding_transcript_exon_variant_and_Consequence_n.vcf"
+#fname = "MoreStringent_jVTFhw6xfxgKfarh.Consequence_ne_downstream_gene_variant_and_Consequence_ne_upstream_gene_variant_and_Consequence_ne_intron_variant_and_Consequence_ne_synonymous_variant_and_Consequence_ne_non_coding_transcript_exon_variant_and_Consequen.vcf"
+fname = "jVTFhw6xfxgKfarh.Consequence_is_missense_variant.vcf"
+with_gnomAD = 0
+threshold = 0.00002
 
 # =============================================================================
 # Helper functions
 # =============================================================================
 def apply_filter(data):
-    for filter_item in consequence:
-        if filter_item in data:
-            return ""
+    global with_gnomAD, threshold
+    msg = ""
+    #return data.split("\t")[7].split("|")[43]
+    gnomAD_AF = data.split("\t")[7].split("|")[43]
+    #print(type(gnomAD_AF))
+    #try:
+        #print(gnomad_AF.isdigit())
+    #    return float(gnomAD_AF)
+    #except:
+    #    return(data)
+    
+    if gnomAD_AF == "": 
+        #return data
+        msg += data
+    else:
+        #return(float(gnomAD_AF))
+        #if float(gnomAD_AF) < .5: return data
+        if float(gnomAD_AF) < threshold: 
+            msg += data
+            with_gnomAD += 1
+    return msg
+    
         
-        #OR
-        
-        try:
-            x = float(data.split("\t")[7].split("|")[42])
-            if x == "": print("NADA")
-            if x > 0.01:
-                #print([x])
-                return ""
-        except:
-            pass
-        
-    return data
-
-
-
 def read_vcf(filename):
     count = 0
-    header = ""
-    variants = ""
-    numvariants = 0
+    #header = ""
+    num_variants = 0
     
     with open(filename) as f:
         while True:
             line = f.readline().strip()
             if line == "": break
-            if count == 10000000000: break #start at 399
+            #if count == 333: break  #starts at 333
             
-            header += line + "\n"
-            
+            if line[0] == "#": 
+                #header += line + "\n"
+                print(line)
+                
             if line[0] != "#":
-                x = apply_filter(line)
-                #if x == "": continue
-                print(x)
-                
-                numvariants += 1
-                #print(apply_filter(line).split("\t")[7].split("|")[42])
+
                 #print(line)
+                x = apply_filter(line)
+                #try:
+                #    print([x], len(x))
+                #except:
+                #    print([x])
                 
+                if x != "": 
+                    print(x)
+                
+                    #print()
+                    num_variants += 1
+
             count += 1
             
         f.close()
-    return numvariants, count
+    return num_variants, count - 333
 # =============================================================================
 #  MAIN          
 # =============================================================================
             
 total_vars, count = read_vcf(fname)
 
-print("Total number of variants returned:", total_vars, count)
+#print("() Total number of variants returned:", total_vars)
+#print("() Processed:", count)
+#print("() with gnomAD AF:", with_gnomAD)
+#print("() Threshold:", threshold)
